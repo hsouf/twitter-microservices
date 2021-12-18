4e72d7401e34070c9bcfcc92d3de7d59324156f8
Amine JAMAI <mr.maj.2015@gmail.com>
2021-12-18 13:59:55 +0100

    New : add tweet service

---
+++ Twitter-TweetService/src/main/java/ma/ensias/twitter/services/NewTweetInterface.java
@@ -0,0 +1,16 @@
+package ma.ensias.twitter.services;
+
+import ma.ensias.twitter.dto.Timeline;
+import ma.ensias.twitter.entities.User;
+
+import java.util.Set;
+
+
+public interface NewTweetInterface {
+
+  static void newTweet(int idUser, String content)
+  {
+      return ;
+  }
+
+}
